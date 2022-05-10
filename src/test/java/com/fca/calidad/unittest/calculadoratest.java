dbb923d198f71b3abd1a91a824fda9b3f6b03080
Gabriela Figueroa <88362727+technologypage@users.noreply.github.com>
2022-05-09 21:09:56 -0500

    Add files via upload

---
+++ src/test/java/com/fca/calidad/unittest/calculadoratest.java
@@ -0,0 +1,30 @@
+package com.fca.calidad.unittest;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class calculadoratest {
+	public calculadora micalculadora;
+
+	@Before
+	public void setUp() throws Exception {
+		micalculadora = new calculadora();
+	}
+
+	
+
+	@Test
+	public void sumatest() {
+		double resultadoejecucion = micalculadora.suma(1, 3);
+		double resultadoesperado = 4;
+		
+		assertThat(resultadoejecucion, is(resultadoesperado));
+	}
+	
+
+}
+
